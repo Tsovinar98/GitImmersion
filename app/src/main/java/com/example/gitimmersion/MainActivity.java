8460b44c181238b000117af2f5001846ae7cb03d
Tsovinar98 <33967268+Tsovinar98@users.noreply.github.com>
2020-12-16 23:26:17 -0500

    Update MainActivity.java

--- app/src/main/java/com/example/gitimmersion/MainActivity.java
+++ app/src/main/java/com/example/gitimmersion/MainActivity.java
@@ -12,5 +12,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         System.out.println("HELLO I AM MAKING AN EDIT");
+        
+        System.out.println("Made edit on Github");
+        
     }
 }
